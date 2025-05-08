fee4c2b4df8062a1bfdab05db9b5ad3816bc35de
suhye0n <contact.suhyeon@gmail.com>
2025-05-08 23:12:09 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,16 @@
+/**
+ *     author:  stella
+ *     created: 05.06.2025 22:34:46
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "Hello World!" << '\n';
+
+    return 0;
+}
