7f6903c1cd970928e20c485eaee0d72ffcdd81f6
suhye0n <contact.suhyeon@gmail.com>
2025-05-08 23:17:03 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,21 @@
+/**
+ *    author:  suhye0n
+ *    created: 08.05.2025 23:16:02
+**/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int A, B;
+  cin >> A >> B;
+  cout << (A + B) << "\n";
+
+  return 0;
+}
